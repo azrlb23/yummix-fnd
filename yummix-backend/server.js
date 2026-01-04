0ab9837900469c53f30cbbe2f96da8c0fb4eb700
azrlb23 <azrielalbian23@gmail.com>
2026-01-04 22:27:04 +0800

    adding the backend.

---
+++ yummix-backend/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Backend Active!'));
+app.listen(5000, () => console.log('Server running on port 5000'));cd
